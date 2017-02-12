c1df841c3ab725cc3b261e89f88581b9ce141098
Bruno da Luz <brunolabx@gmail.com>
2017-02-12 16:20:56 -0200

    implementation view model

--- PasteMenu/PasteMenu/PasteMenuViewController.swift
+++ PasteMenu/PasteMenu/PasteMenuViewController.swift
@@ -5,7 +5,7 @@ class PasteMenuViewController: NSViewController {
     @IBOutlet weak var linkTextField: NSTextField!
     @IBOutlet weak var tableView: NSTableView!
     
-    var objects = [String]()
+    var viewModel = PasteMenuViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +15,7 @@ class PasteMenuViewController: NSViewController {
         let text = linkTextField.stringValue
         guard text.characters.count > 0 else { return }
         
-        objects.append(text)
+        viewModel.set(link: text)
         linkTextField.stringValue = ""
         tableView.reloadData()
     }
@@ -25,20 +25,20 @@ extension PasteMenuViewController: NSTableViewDataSource, NSTableViewDelegate {
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         let cellView = tableView.make(withIdentifier: "cell", owner: self) as! NSTableCellView
-        cellView.textField?.stringValue = objects[row]
+        cellView.textField?.stringValue = viewModel.get(link: row)
         return cellView
     }
     
     func tableViewSelectionDidChange(_ notification: Notification) {
         
-        let link = self.objects[tableView.selectedRow]
+        let link = viewModel.get(link: tableView.selectedRow)
         
         NSPasteboard.general().clearContents()
         NSPasteboard.general().setString(link, forType: NSPasteboardTypeString)
     }
     
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return self.objects.count
+        return viewModel.numberOfRows()
     }
     
 }
