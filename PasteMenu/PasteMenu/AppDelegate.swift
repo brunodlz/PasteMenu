10f59be310cb4a9db10b437668dd618b9f78f870
Bruno da Luz <brunolabx@gmail.com>
2017-02-11 23:09:46 -0200

    adding status bar with icon

--- PasteMenu/PasteMenu/AppDelegate.swift
+++ PasteMenu/PasteMenu/AppDelegate.swift
@@ -3,11 +3,23 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    fileprivate var statusItem: NSStatusItem!
+    
     @IBOutlet weak var window: NSWindow!
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) { }
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        statusItem.image = NSImage(named: "pastemenu")
+        statusItem.target = self
+        statusItem.action = #selector(showPaste(_:))
+        statusItem.highlightMode = true
+    }
 
     func applicationWillTerminate(_ aNotification: Notification) { }
 
+    @objc fileprivate func showPaste(_ sender: NSStatusBarButton) {
+        print("it's working !!!")
+    }
+    
 }
 
