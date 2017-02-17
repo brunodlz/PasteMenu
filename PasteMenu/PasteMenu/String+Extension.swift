13b8da6a4b09c0c5e5c8e6be60efff5a65ae0ada
Bruno da Luz <brunolabx@gmail.com>
2017-02-17 02:09:37 -0200

    adding regex for validation

--- PasteMenu/PasteMenu/String+Extension.swift
+++ PasteMenu/PasteMenu/String+Extension.swift
@@ -1,1 +1,36 @@
 import Foundation
+
+extension String {
+    
+    func containsAudio() -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "\\.mp(3|4)$", options: [.caseInsensitive])
+            return regex.numberOfMatches(in: self, options: [],
+                                         range: NSMakeRange(0, self.characters.count)) > 0
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    func containsVideo() -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "\\.(avi|mkv)$", options: [.caseInsensitive])
+            return regex.numberOfMatches(in: self,
+                                         options: [], range: NSMakeRange(0, self.characters.count)) > 0
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+ 
+    func containsLink() -> Bool {
+        let urlRegEx = "((?:http|https)://)?(?:www\\.)?[\\w\\d\\-_]+\\.\\w{2,3}(\\.\\w{2})?(/(?<=/)(?:[\\w\\d\\-./_]+)?)?"
+        return NSPredicate(format: "SELF MATCHES %@", urlRegEx).evaluate(with: self)
+    }
+    
+    func containsEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let predicate = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return predicate.evaluate(with: self)
+    }
+    
+}
